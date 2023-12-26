683075e33a124839aa5be1059d4bef357989320e
Awesomeplayer165 <dev@awesomeplayer.tech>
2023-12-25 21:53:59 -0800

    use drive subsystem run to handle function implicitly

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -59,13 +59,12 @@ public class RobotContainer {
     m_robotDrive.setDefaultCommand(
         // The left stick controls translation of the robot.
         // Turning is controlled by the X axis of the right stick.
-        new RunCommand(
-            () -> m_robotDrive.drive(
-                m_driverController.getLeftY(),
-                m_driverController.getLeftX(),
-                m_driverController.getRightX(),
-                false),
-            m_robotDrive));
+        m_robotDrive.run(() -> m_robotDrive.drive(m_driverController.getLeftY(),
+            m_driverController.getLeftX(),
+            m_driverController.getRightX(),
+            false)
+        )
+      );
   }
 
   /**
